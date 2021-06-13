abd84714b3c1c679ba0c14567142e7dcba4f5864
junnys6018 <junkanglim1@gmail.com>
2021-06-13 20:48:06 +0800

    finished project

--- preload/disposal_fee_dialog.js
+++ preload/disposal_fee_dialog.js
@@ -1,6 +1,14 @@
 const ipcRenderer = require('electron').ipcRenderer;
 const { getSettings, writeSettings } = require('../settings.js');
 
+function isNumeric(str) {
+	if (typeof str != "string") {
+		return false
+	}
+
+	return !isNaN(str) && !isNaN(parseFloat(str))
+}
+
 window.addEventListener('DOMContentLoaded', _ => {
 	const disposalFeeInput = document.getElementById('disposal-fee');
 	let settings = getSettings();
@@ -10,19 +18,31 @@ window.addEventListener('DOMContentLoaded', _ => {
 	document.getElementById('cancel').addEventListener('click', _ => ipcRenderer.send('close-dialog'));
 
 	document.getElementById('save').addEventListener('click', _ => {
-		settings.disposalFee = Number(Number(disposalFeeInput.value).toFixed(2));
-		if (settings.disposalFee >= 0) {
-			writeSettings(settings);
-			ipcRenderer.send('close-dialog');
-			ipcRenderer.send('update-disposal-fee');
-		}
-		else {
+		function error(message) {
 			document.getElementById('error-popup').style.visibility = 'visible';
+			document.getElementById('error-popup-message').innerHTML = message;
 		}
+
+		if (!isNumeric(disposalFeeInput.value)) {
+			error('Invalid Input');
+			return;
+		}
+
+		settings.disposalFee = Number(Number(disposalFeeInput.value).toFixed(2));
+
+		if (settings.disposalFee < 0) {
+			error('Negative Disposal Fee')
+			return;
+		}
+		writeSettings(settings);
+		ipcRenderer.send('close-dialog');
+		ipcRenderer.send('update-disposal-fee');
 	});
 
 	document.getElementById('disposal-fee').addEventListener('blur', _ => {
-		let fee = Number(disposalFeeInput.value);
-		disposalFeeInput.value = fee.toFixed(2);
+		if (isNumeric(disposalFeeInput.value)) {
+			let fee = Number(disposalFeeInput.value);
+			disposalFeeInput.value = fee.toFixed(2);
+		}
 	})
 });
--- preload/index.js
+++ preload/index.js
@@ -1,15 +1,126 @@
 const { ipcRenderer } = require('electron');
 const { getSettings } = require('../settings.js');
 
-function updateDisposalFeebadge() {
-	const settings = getSettings();
+function updateDisposalFeebadge(value) {
 	let disposalFeeBadge = document.getElementById('disposal-fee-badge');
-	disposalFeeBadge.innerText = '$' + settings.disposalFee.toFixed(2);
-
+	disposalFeeBadge.innerText = '$' + value.toFixed(2);
 }
 
-window.addEventListener('DOMContentLoaded', _ => {
-	updateDisposalFeebadge();
+let cachedDisposalFee = getSettings().disposalFee;
 
-	ipcRenderer.on('update-disposal-fee', updateDisposalFeebadge);
+window.addEventListener('DOMContentLoaded', _ => {
+	updateDisposalFeebadge(cachedDisposalFee);
+
+	// Setup forms
+	function isNumeric(str) {
+		if (typeof str !== 'string') {
+			return false
+		}
+
+		return !isNaN(str) && !isNaN(parseFloat(str))
+	}
+
+	class Calculator {
+		constructor(inputElements, outputElements, handler) {
+			this.inputElements = inputElements;
+			this.outputElements = outputElements;
+			this.handler = handler;
+			
+			function setupInputHandlers(inputElement) {
+				inputElement.addEventListener('blur', _ => {
+					if (isNumeric(inputElement.value)) {
+						let value = Number(inputElement.value);
+						inputElement.value = value.toFixed(2);
+					}
+				})
+				
+				inputElement.addEventListener('input', this.updateForm.bind(this));
+			}
+			
+			for (let inputElement of inputElements) {
+				setupInputHandlers.bind(this)(inputElement)
+			}
+		}
+
+		updateForm() {
+			const allInputsValid = this.inputElements.every(e => isNumeric(e.value));
+
+			if (allInputsValid) {
+				const inputs = this.inputElements.map(e => Number(e.value));
+				this.handler(...inputs);
+			}
+			else {
+				for (let outputElement of this.outputElements) {
+					outputElement.value = '';
+				}
+			}
+		}
+	}
+
+	function writeOutput(element, value) {
+		element.value = value.toFixed(2);
+	}
+
+	// ----------------------------- Discount ----------------------------- //
+
+	let systemCost1 = document.getElementById('i-system-cost-1');
+	let discount = document.getElementById('i-discount');
+
+	let discountEx1 = document.getElementById('o-discount-ex-1');
+	let discountInc1 = document.getElementById('o-discount-inc-1');
+
+	let discountCalculator = new Calculator([systemCost1, discount], [discountEx1, discountInc1], (systemCostVal, discountVal) => {
+		const discountExVal = (systemCostVal - cachedDisposalFee) * (100 - discountVal) / 100 + cachedDisposalFee;
+		const discountIncVal = discountExVal * 1.1;
+
+		writeOutput(discountEx1, discountExVal);
+		writeOutput(discountInc1, discountIncVal);
+	});
+
+	// ----------------------------- Cashback ----------------------------- //
+
+	let systemCost2 = document.getElementById('i-system-cost-2');
+	let cashBack = document.getElementById('i-cash-back');
+
+	let discountEx2 = document.getElementById('o-discount-ex-2');
+	let discountInc2 = document.getElementById('o-discount-inc-2');
+
+	let cashBackCalculator = new Calculator([systemCost2, cashBack], [discountEx2, discountInc2], (systemCostVal, cashBackVal) => {
+		const discountExVal = systemCostVal - cashBackVal / 1.1;
+		const discountIncVal = discountExVal * 1.1;
+
+		writeOutput(discountEx2, discountExVal);
+		writeOutput(discountInc2, discountIncVal);
+	});
+
+	// ----------------------------- Gross Profit ----------------------------- //
+
+	let costPrice = document.getElementById('i-cost-price');
+	let sellPriceInc = document.getElementById('i-sell-price-inc');
+
+	let sellPriceEx = document.getElementById('o-sell-price-ex');
+	let grossProfitEx = document.getElementById('o-gross-profit-ex');
+	let grossProfitP = document.getElementById('o-gross-profit-p');
+	let grossLevy = document.getElementById('o-gross-levy');
+
+	let grossProfitCalculator = new Calculator([costPrice, sellPriceInc], [sellPriceEx, grossProfitP, grossProfitEx, grossLevy], (costPriceVal, sellPriceIncVal) => {
+		const sellPriceExVal = sellPriceIncVal / 1.1;
+		const grossProfitExVal = sellPriceExVal - costPriceVal;
+		const grossProfitPVal = 100 * grossProfitExVal / sellPriceExVal;
+		const grossLevyVal = grossProfitExVal - (0.055 * sellPriceExVal);
+
+		writeOutput(sellPriceEx, sellPriceExVal);
+		writeOutput(grossProfitEx, grossProfitExVal);
+		writeOutput(grossProfitP, grossProfitPVal);
+		writeOutput(grossLevy, grossLevyVal);
+	});
+
+	ipcRenderer.on('update-disposal-fee', _ => {
+		const settings = getSettings();
+		updateDisposalFeebadge(settings.disposalFee);
+		cachedDisposalFee = settings.disposalFee;
+
+		// Update discount form based on new value
+		discountCalculator.updateForm();
+	});
 });
